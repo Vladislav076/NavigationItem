b246890de5a2b2be6ff9b05c6d418f5ef5df55ba
Vladislav Densiov <denisov076@gmail.com>
2023-05-24 17:49:43 +0300

    Initial Commit

---
+++ NavigationItem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NavigationItem
+//
+//  Created by Vladislav Denisov on 24.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
